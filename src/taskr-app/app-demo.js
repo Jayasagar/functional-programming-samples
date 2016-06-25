67f547c0c5d031c3e98d8c5a67c2793314646d5f
Jayasagar <jayasagar@jayasagars-mbp.localdomain>
2016-06-25 17:53:34 +0200

    Now stream of events works !!!

--- src/taskr-app/app-demo.js
+++ src/taskr-app/app-demo.js
@@ -11,10 +11,21 @@ export const streamOfActions = [
    addTask('Fill in timesheets for the week of 16th of June', nextWeek())
 ];
 
+const processEvent = (previousEvent, currentEvent) => {
+    Taskr.dispatch(previousEvent);
+    return currentEvent;
+};
+
 const simulateApp = () => {
     // Prepare stream of dispatch events
     // add few tasks, completeTask
-    Taskr.dispatch(streamOfActions[0]);
+    streamOfActions.reduce(processEvent);
+
+//    Taskr.dispatch(streamOfActions[0]);
+//    Taskr.dispatch(streamOfActions[1]);
+//    Taskr.dispatch(streamOfActions[2]);
+//    Taskr.dispatch(streamOfActions[3]);
+
     Taskr.viewAllTasks();
 };
 
